470c03dcb1bdf47a263c7d1475b74e70079b4c84
KiaMasoudifar <145088582+KiaMasoudifar@users.noreply.github.com>
2024-05-31 22:53:02 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+let linkChange = document.getElementById("link")
+
+linkChange.href = "https://www.aljazeera.com"
+
+
+anchorElement = document.querySelector("#link")
+
+anchorElement.href = "https://www.google.com"
